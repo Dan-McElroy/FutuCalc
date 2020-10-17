32b213be64454e281e0353a4efd0f77f2ca19489
Dan McElroy <daniel@mcelroys.net>
2020-10-17 16:13:19 +0200

    Cleaned up API project, fixed DI to inject correct calculator.

--- FutuCalc.API/Startup.cs
+++ FutuCalc.API/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FutuCalc.Core.Calculation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace FutuCalc.API
 {
@@ -27,7 +20,7 @@ namespace FutuCalc.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddScoped<ICalculator, SimpleCastCalculator>();
+            services.AddScoped<ICalculator, Calculator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
